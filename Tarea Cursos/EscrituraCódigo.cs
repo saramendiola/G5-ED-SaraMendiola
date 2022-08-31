6cff0e43cadec23f6c3bd2897f01f7bf7b665633
Sara Mendiola <saramendiola@MacBook-Pro-de-Sara.local>
2022-08-31 15:45:23 -0700

    cursos básicos de programación

---
+++ Tarea Cursos/EscrituraCódigo.cs
@@ -0,0 +1,5 @@
+//este es el reto del primer curso de escritura de código en C#
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is");
+Console.Write(" the second line");
---
+++ Tarea Cursos/OperacionesBásicas.cs
@@ -0,0 +1,5 @@
+//este es el reto del curso de operaciones básicas en C# que hicimos de tarea:)
+
+int fahrenheit = 94;
+decimal respuesta = (fahrenheit - 32)*(5m/9m);
+Console.WriteLine("The temperature is " + respuesta + " Celsius");
